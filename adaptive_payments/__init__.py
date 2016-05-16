f11589beb0bf476cc38f7582310195da14b93953
Sayed Khader <sy@sayedkhader.com>
2016-05-16 00:31:52 -0400

    more init stuff

--- adaptive_payments/__init__.py
+++ adaptive_payments/__init__.py
@@ -1,2 +1,2 @@
 #!/usr/bin/env python
-from adaptive_payments import get_refresh_token, refresh_access_token, get_user_id
+from adaptive_payments import get_refresh_token, refresh_access_token, get_user_identity
