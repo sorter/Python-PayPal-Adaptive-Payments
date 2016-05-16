390501c7da2a3567dd694259b3a786c0d998b0b9
Sayed Khader <sy@sayedkhader.com>
2016-05-16 00:21:46 -0400

    favouring email-based payouts instead

--- adaptive_payments/adaptive_payments.py
+++ adaptive_payments/adaptive_payments.py
@@ -37,7 +37,7 @@ def refresh_access_token(refresh):
     r = requests.post(url,headers=headers,data=data,auth=(user,passwd))
     return json.loads(r.content)["access_token"]
 
-def user_id_payment(amt, receiver_id):
+def make_payment(amt, receiver_email):
     """make payment, assumes paypal security credentials belong to the payer"""
 
     headers = {
@@ -52,40 +52,21 @@ def user_id_payment(amt, receiver_id):
             "erList.receiver(0).accountId=%s&receiverList.receiver(0).amount=%s&req"
             "uestEnvelope.errorLanguage=en_US&returnUrl=%s") 
             % (PAYPAL_CONFIG['sender_email'], PAYPAL_CONFIG['cancelUrl'], 
-               receiver_id, amt, PAYPAL_CONFIG['returnUrl']))
+               receiver_email, amt, PAYPAL_CONFIG['returnUrl']))
     #url = PAYPAL_CONFIG['api_url'] + '/' + ENDPOINT_URI
     url = '%s%s' % (SANDBOX_SOAP_URL, ENDPOINT_URI)
     r = requests.post(url, headers=headers, data=data)
     return r
 
 
-def make_payment(amt, receiver_email):
-    """make payment, assumes paypal security credentials belong to the payer"""
-
-    headers = {
-        "X-PAYPAL-SECURITY-USERID": PAYPAL_CONFIG["api_username"], 
-        "X-PAYPAL-SECURITY-PASSWORD": PAYPAL_CONFIG["api_password"],
-        "X-PAYPAL-SECURITY-SIGNATURE": PAYPAL_CONFIG["api_signature"],
-        "X-PAYPAL-REQUEST-DATA-FORMAT": "NV",
-        "X-PAYPAL-RESPONSE-DATA-FORMAT": "NV" ,
-        "X-PAYPAL-APPLICATION-ID": PAYPAL_CONFIG["app_id"]} 
-
-    data= (("actionType=PAY&senderEmail=%s&cancelUrl=%s&currencyCode=USD&receiv"
-            "erList.receiver(0).email=%s&receiverList.receiver(0).amount=%s&req"
-            "uestEnvelope.errorLanguage=en_US&returnUrl=%s") 
-            % (PAYPAL_CONFIG['sender_email'], PAYPAL_CONFIG['cancelUrl'], 
-               receiver_email, amt, PAYPAL_CONFIG['returnUrl']))
-    url = PAYPAL_CONFIG['api_url'] + '/' + ENDPOINT_URI
-    r = requests.post(url, headers=headers, data=data)
-
 u = 'https://api.sandbox.payypal.com/v1/identiy/openidconnect/userinfo/?schema=openid'
 access_token = 'A015MPrEAeZL6c9hxHK-9zQV3lJj1fP0MMXADcHQ6tpsKjY'
 
 
-def get_user_id(access_token):
+def get_user_identity(access_token):
     u = ('https://api.sandbox.paypal.com/v1/identity/openidconnect/userinfo/'
              '?schema=openid')
     headers = { 'Content-Type': 'application/json', 
                 'Authorization': 'Bearer %s' % access_token}
     r = requests.get(u,headers=headers)
-    return json.loads(r.content)["user_id"].replace('https://www.paypal.com/webapps/auth/identity/user/', '')
+    return json.loads(r.content)
